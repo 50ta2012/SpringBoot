4002ae040ecf94f45777afe22df42575d52c4d02
Berlin <50ta2012@gmail.com>
2024-04-25 11:21:47 +0800

    mssql profile getter setter output

--- mssql/src/main/java/com/example/mssql/MssqlApplication.java
+++ mssql/src/main/java/com/example/mssql/MssqlApplication.java
@@ -1,13 +1,33 @@
 package com.example.mssql;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.Bean;
+
+// import com.example.mssql.entity.Profile;
+import com.example.mssql.entity.ProfileRepository;
 
 @SpringBootApplication
 public class MssqlApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(MssqlApplication.class, args);
-	}
+  public static void main(String[] args) {
+    SpringApplication.run(MssqlApplication.class, args);
+  }
+
+  @Autowired
+  ProfileRepository profileRepository;
+
+  @Bean
+  CommandLineRunner commandLineRunner(ApplicationContext ctx) {
+    return arg -> {
+      // Profile profile = new Profile("Berlin", 30, "Rotary");
+      // System.out.println(profileRepository.save(profile));
+
+      // System.out.println(profileRepository.findAll());
+    };
+  }
 
 }
--- mssql/src/main/java/com/example/mssql/entity/Profile.java
+++ mssql/src/main/java/com/example/mssql/entity/Profile.java
@@ -14,4 +14,57 @@ public class Profile {
   String name;
   Integer age;
   String tag;
+
+  public Integer getId() {
+    return this.id;
+  }
+
+  public void setId(Integer id) {
+    this.id = id;
+  }
+
+  public String getName() {
+    return this.name;
+  }
+
+  public void setName(String name) {
+    this.name = name;
+  }
+
+  public Integer getAge() {
+    return this.age;
+  }
+
+  public void setAge(Integer age) {
+    this.age = age;
+  }
+
+  public String getTag() {
+    return this.tag;
+  }
+
+  public void setTag(String tag) {
+    this.tag = tag;
+  }
+
+  public Profile(String name, Integer age, String tag) {
+    this.name = name;
+    this.age = age;
+    this.tag = tag;
+  }
+
+  public Profile() {
+
+  }
+
+  @Override
+  public String toString() {
+    return "{" +
+        " id='" + getId() + "'" +
+        ", name='" + getName() + "'" +
+        ", age='" + getAge() + "'" +
+        ", tag='" + getTag() + "'" +
+        "}";
+  }
+
 }
